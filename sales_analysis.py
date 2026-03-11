df87c32467c0fc8b837bcbc726d41c5ccee2d224
Mitney <zhoumitchell@yahoo.com>
2026-03-11 11:14:10 +0100

    Add files via upload

---
+++ sales_analysis.py
@@ -0,0 +1,10 @@
+# Simple Sales Analysis Example
+sales = [120, 340, 560, 220, 150, 430]
+total_sales = sum(sales)
+average_sales = total_sales / len(sales)
+highest_sale = max(sales)
+lowest_sale = min(sales)
+print("Average Sales:", average_sales)
+print("Total Sales:", total_sales)
+print("Highest Sale:", highest_sale)
+print("Lowest Sale:", lowest_sale)
